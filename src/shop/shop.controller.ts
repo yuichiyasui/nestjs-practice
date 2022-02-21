b116ef843655b96a2169f2d061052fa0ec48e758
yuichiyasui <yassan.b.a.w@gmail.com>
2022-02-22 01:35:24 +0900

    exception filterを作成

--- src/shop/shop.controller.ts
+++ src/shop/shop.controller.ts
@@ -1,19 +1,33 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpException,
+  HttpStatus,
+  Param,
+  Post,
+  UseFilters,
+} from '@nestjs/common';
 import { CreateShopDto } from 'src/dto/create-shop.dto';
+import { ForbiddenException } from 'src/forbbiden.exception';
+import { HttpExceptionFilter } from 'src/http-exception.filter';
 import { Shop } from 'src/interfaces/shop.interface';
 import { ShopService } from './shop.service';
 
 @Controller('shop')
+@UseFilters(new HttpExceptionFilter())
 export class ShopController {
   constructor(private shopService: ShopService) {}
 
   @Post()
   create(@Body() createShopDto: CreateShopDto): string {
+    throw new ForbiddenException();
     return `「${createShopDto.name}」を作成しました`;
   }
 
   @Get()
   findAll(): Shop[] {
+    throw new ForbiddenException();
     return this.shopService.findAll();
   }
 
