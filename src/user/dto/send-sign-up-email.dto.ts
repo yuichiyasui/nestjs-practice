13b39df9268249da7095eb8791daf0230609e6d5
yuichiyasui <yassan.b.a.w@gmail.com>
2023-04-01 19:27:11 +0900

    Add validation to email

--- src/user/dto/send-sign-up-email.dto.ts
+++ src/user/dto/send-sign-up-email.dto.ts
@@ -1,6 +1,8 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail } from 'class-validator';
 
 export class SendSignUpEmailDto {
   @ApiProperty()
+  @IsEmail()
   readonly email!: string;
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -6,8 +6,6 @@ export class UserService {
   constructor(private mailerService: MailerService) {}
 
   sendSignUpEmail({ email }: { email: string }) {
-    if (!email.endsWith('@gmail.com')) return;
-    // TODO: メールアドレスの形式を検証する
     this.mailerService.sendMail({
       to: email,
       subject: '[Todo App] ユーザー登録',
